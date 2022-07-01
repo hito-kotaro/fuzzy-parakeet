f2ab3b5be9dc03da7beab373a56d191de5658894
hito-kotaro <tohi.work@gmail.com>
2022-07-01 12:47:34 +0900

    フォーカス時のアウトライン表示を無効化

--- src/components/atoms/Buttons/PrimaryButton/PrimaryButton.tsx
+++ src/components/atoms/Buttons/PrimaryButton/PrimaryButton.tsx
@@ -9,7 +9,11 @@ type Props = {
 const PrimaryButton: VFC<Props> = (props) => {
   const { children, thema, onClick } = props;
   return (
-    <button type="button" onClick={onClick} className={`${thema} rounded-lg text-2xl p-2 w-full`}>
+    <button
+      type="button"
+      onClick={onClick}
+      className={`${thema} rounded-lg text-2xl p-2 w-full focus:outline-none`}
+    >
       {children}
     </button>
   );
