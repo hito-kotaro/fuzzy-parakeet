3232e2288bcca1cde5364f44be1495c66d01579f
hito-kotaro <tohi.work@gmail.com>
2022-07-02 00:46:57 +0900

    フッター作成(試作)

---
+++ src/components/organisms/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer = () => {
+  return <div className=" fixed bottom-0 bg-base h-10 w-full" />;
+};
+
+export default Footer;
--- src/components/organisms/Headers/CategoryHeader.tsx
+++ src/components/organisms/Headers/CategoryHeader.tsx
@@ -3,6 +3,7 @@ import { IoIosAdd } from 'react-icons/io';
 import SearchForm from '../../atoms/InputForms/SearchForm/SearchForm';
 import SelectForm from '../../atoms/InputForms/SelectForm/SelectForm';
 import HeaderLink from '../../atoms/Links/HeaderLink';
+import Footer from '../Footer/Footer';
 
 type Props = {
   title: string;
@@ -38,6 +39,7 @@ const CategoryHeader: VFC<Props> = (props) => {
           </div>
         </div>
       </div>
+      <Footer />
     </>
   );
 };
