707de73bea38e448637105df820fd3382a70396c
hito-kotaro <tohi.work@gmail.com>
2022-07-02 14:32:43 +0900

    不要importの削除

--- src/components/molecules/Lists/PrimaryList.tsx
+++ src/components/molecules/Lists/PrimaryList.tsx
@@ -1,5 +1,4 @@
 import React, { VFC } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { primaryListItem } from '../../../types/ListItem/PrimaryListItemType';
 import PrimaryListItemButton from '../../atoms/Buttons/PrimaryListItemButton';
 
@@ -9,8 +8,7 @@ type Props = {
 
 const PrimaryList: VFC<Props> = (props) => {
   const { list } = props;
-  const navigate = useNavigate();
-  const id = 1;
+
   return (
     <>
       {list?.map((item: primaryListItem) => (
