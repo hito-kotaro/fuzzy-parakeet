ddf2508c8e51f5611f7577bfa91bff1def34eed2
hito-kotaro <tohi.work@gmail.com>
2022-07-04 22:54:19 +0900

    丸にならない不具合の修正

--- src/components/atoms/Buttons/PrimaryListItemButton.tsx
+++ src/components/atoms/Buttons/PrimaryListItemButton.tsx
@@ -1,5 +1,4 @@
 import React, { ReactElement, VFC } from 'react';
-import { detailTemplateType } from '../../../types/detailTemplateType';
 import MyAvatar from '../Avatar/MyAvatar';
 
 type Props = {
@@ -15,30 +14,19 @@ type Props = {
 const PrimaryListItemButton: VFC<Props> = (props) => {
   const { id, name, title, description, date, badge, onClick } = props;
   const isNoText = description.length === 0;
-  const detailHeaderData: detailTemplateType = {
-    id,
-    name,
-    title,
-    description,
-    date,
-    status: true,
-    message: 'test',
-  };
 
   return (
     <button type="button" className="w-full" onClick={() => onClick(id)}>
       <div className="flex border-b-1 px-3 py-2 bg-white">
-        <div className="w-1/5 ">
-          <MyAvatar size={44} name={name} />
-        </div>
+        <MyAvatar size={44} name={name} />
 
-        <div className="w-3/5 text-left">
+        <div className="ml-5 text-left">
           <div className="text-sm text-gray-500 whitespace-nowrap truncate">{title}</div>
           <div className={`text-lg  whitespace-nowrap truncate ${isNoText ? 'text-gray-400' : ''}`}>
             {isNoText ? 'No Text' : description}
           </div>
         </div>
-        <div className="text-right text-gray-400">
+        <div className="ml-auto text-right text-gray-400">
           <div>{date}</div>
           <div>{badge}</div>
         </div>
