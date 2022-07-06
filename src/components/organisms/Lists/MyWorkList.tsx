17eaaa8b5376114ab72547d3d3f193a36b42fc91
hito-kotaro <tohi.work@gmail.com>
2022-07-07 00:09:22 +0900

    timeline廃止

--- src/components/organisms/Lists/MyWorkList.tsx
+++ src/components/organisms/Lists/MyWorkList.tsx
@@ -30,13 +30,6 @@ const MyWorkList = () => {
 
       <div className={`h-${space}`} />
 
-      <IconLabelButton
-        text="TimeLine"
-        icon={<AiOutlineHistory />}
-        iconColor="bg-yellow-400"
-        path="/timeline"
-      />
-
       <div className={`h-${space}`} />
 
       <IconLabelButton
